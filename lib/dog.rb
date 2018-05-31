c8d7a59445754746f9985b0f1e08f66953fb8034
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-31 08:32:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+
+class Dog 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    new_name = name 
+    @name - new_name
+  end
+  
+  
+  def bark 
+    puts "woof!"
+  end
+  
+end
